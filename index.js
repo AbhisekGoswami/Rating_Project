9c3bfaa563205e947b731d4938ab04930df4991f
AbhisekGoswami <78299459+AbhisekGoswami@users.noreply.github.com>
2023-08-27 14:00:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let container=document.getElementById("star-container");
+let star=document.querySelectorAll(".star");
+let filled=0;
+
+container.addEventListener("click",function(e){
+    let currClicked=e.target.dataset.index;
+    
+    for(let i=0;i<star.length;i++){
+        star[i].classList.remove("star-filled");
+    }
+
+    for(let i=0;i<currClicked;i++){
+        star[i].classList.add("star-filled");
+    }
+
+    document.getElementById("count").innerText=`Rating Count :${currClicked}`;
+
+    filled=currClicked;
+})
+
+container.addEventListener("mouseover",function(e){
+    let currClicked=e.target.dataset.index;
+    
+    for(let i=0;i<currClicked;i++){
+        star[i].classList.add("star-filled");
+    }
+
+})
+
+container.addEventListener("mouseleave",function(e){
+    let currClicked=e.target.dataset.index;
+    
+    for(let i=filled;i<star.length;i++){
+        star[i].classList.remove("star-filled");
+    }
+})
+
